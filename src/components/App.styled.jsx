4eb1efb8830899ea2c4fe1127dc506887300143f
Karina-98 <115569415+Karina-98@users.noreply.github.com>
2023-07-19 12:19:54 +0200

    +

--- src/components/App.styled.jsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-width: 400px;
-padding: 10px;
-margin: 0 auto;
-`
--- src/components/Title/Title.jsx
+++
@@ -1,5 +0,0 @@
-import { Title } from "./Title.styled"
-
-export const Titles = ({children}) => {
-    return <Title>{children}</Title>
-}
